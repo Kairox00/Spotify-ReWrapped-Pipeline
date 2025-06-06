3a75856662adf12fd0439f133668b59bbdb96134
Khaled <khaled.romeh@archerirm.com>
2025-06-06 23:44:09 +0300

    Moved pipeline into functions

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,11 +1,18 @@
 import sys
+import pipeline
 def handler(event, context):
   if '.' not in sys.path:
     sys.path.insert(0, '.')
 
-  import pipeline
-
-  return {
-    "statusCode": 200,
-    "body": "Hello from Lambda!"
-  }
+  try:
+    pipeline.main()
+    return {
+      "statusCode": 200,
+      "body": "Success!"
+    }
+  except Exception as e:
+    print(e)
+    return {
+      "statusCode": 500,
+      "body": str(e)
+    }
