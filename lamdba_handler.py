b4d956d3582de3bd5bef6a7b1108cbf094fce41f
Khaled <khaled.romeh@archerirm.com>
2025-06-06 21:52:33 +0300

    Added lambda handler

---
+++ lamdba_handler.py
@@ -0,0 +1,11 @@
+import sys
+def handler(event, context):
+  if '.' not in sys.path:
+    sys.path.insert(0, '.')
+
+  import pipeline
+
+  return {
+    "statusCode": 200,
+    "body": "Hello from Lambda!"
+  }
